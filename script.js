b2b46424422ba239a51be8a1145108879646a3f4
Yusuf007R <45109516+Yusuf007R@users.noreply.github.com>
2020-04-01 14:37:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ getcanvas.width = 1000
 getcanvas.height = 1000
 var v1
 var v2
-var digit = 3
+var digit = 6
 var col = 0
-const timestep =100
+const timestep =100000
 class cuadrado {
     constructor(x, y, w, h, s, c, m) {
         this.x = x
